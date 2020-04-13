e33442519b61a7546e0f8c513d127db3752591e9
Anders Djerf <37343667+istorp@users.noreply.github.com>
2020-04-13 03:09:32 +0200

    glömde sava ju

--- TrainingAgin/TrainingAgin/Program.cs
+++ TrainingAgin/TrainingAgin/Program.cs
@@ -1,14 +1,18 @@
-﻿using Microsoft.Extensions.Hosting;
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
 using System;
 
 namespace TrainingAgin
 {
     class Program
     {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello World!");
-        }
-        public static IHostBuilder
+        static void Main(string[] args) => CreateHostBuilder(args).Build().Run();
+        public static IHostBuilder CreateHostBuilder(string[] args)
+        => Host.CreateDefaultBuilder(args)
+            .ConfigureServices((hostContext,services)=>
+            {
+                services.AddHostedService<mainService>();
+            });
+
     }
 }
--- TrainingAgin/TrainingAgin/mainService.cs
+++ TrainingAgin/TrainingAgin/mainService.cs
@@ -1,10 +1,25 @@
-﻿using System;
+﻿using Microsoft.Extensions.Hosting;
+using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
 
 namespace TrainingAgin
 {
-    class mainService
+    public class mainService : IHostedService
     {
+        public Task StartAsync(CancellationToken cancellationToken)
+        {
+            Console.WriteLine("Hallo Anders");
+            Console.ReadKey();
+            return Task.CompletedTask;
+
+        }
+
+        public Task StopAsync(CancellationToken cancellationToken)
+        {
+            return Task.CompletedTask;
+        }
     }
 }
